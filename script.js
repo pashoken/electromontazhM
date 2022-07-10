a0616206aaf0c1f013229506fdadec5f9d4dddd9
pashoken <pashaperevalov6@gmail.com>
2022-07-10 10:23:08 +0400

    auf

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+ $('a[href^="#"]').click(function(){
+   let anchor = $(this).attr('href');
+   $('html, body').animate({
+     scrollTop:  $(anchor).offset().top
+   }, 600);
+});
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
